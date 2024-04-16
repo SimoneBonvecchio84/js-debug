832e6489db280f21377d6ebaf5a43420ad5ebe0a
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-16 22:31:52 +0200

    Fix: debug-objects

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -11,6 +11,93 @@
 
 
 // ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+// const cars = [
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Fiesta',
+//         type: 'diesel'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'A1',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Golf',
+//         type: 'Benzina'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Panda',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Multipla',
+//         type: 'GPL'
+//     },
+//     {
+//         manufacturer: 'Tesla',
+//         model: 'Model 3',
+//         type: 'elettrico'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Polo',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Kuga',
+//         type: 'Diesel'
+//     },
+//     {
+//         manufacturer: 'Seat',
+//         model: 'Ibiza',
+//         type: 'metano'
+//     }
+//     {
+//         manufacturer: 'Audi',
+//         model: 'R8',
+//         type: 'Benzina'
+//     },
+// ];
+
+// const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+
+// const dieselCars = cars.filter( (auto) => {
+//     auto.type === 'diesel';
+// });
+
+// const otherCars = cars.filter( (auto) => {
+//     return auto.type !== 'benzina' || auto.type !== 'diesel';
+// });
+
+// console.log('Auto a benzina');
+// console.log('*******************************');
+// console.log(gasolineCars);
+
+// console.log('Auto a diesel');
+// console.log('*******************************');
+// console.log(dieselCars);
+
+// console.log('Tutte le altre auto');
+// console.log(otherCars);
+
+//-----------------------------------------------------------------------------------------
+// In questo codice è presente un array di oggetti nei quali abbiamo 3 chiavi con
+// Nome della Marca dell'automobile, il suo modello e il tipo di carburante.
+// Poi abbiamo tre costanti nelle quali tratteremo l'array car con il metodo filter
+// che ci restituirà 3 array nelle quali verrano inseriti i modelli in base alla tipologia di carburante
+// Errori sintattici
+// Non c'è la virgola che separa l' oggetto seat dall'oggetto audi
+// L'arrow function nella prima costante è scritta in modo errato, ovvero è stao messo prima 
+// il simbolo del maggiore e poi l' uguale e mancano anche le graffe che per contenere la condizione.
+// nel ultima condizione invece che or || ho preferito inserire and cosi le macchine sia diverse da
+// benzina che da diesel saranno inserite nell array otherCars.
+// In più filter necessita di return che non è presente nei primi due filter.
+
 const cars = [
     {
         manufacturer: 'Ford',
@@ -56,7 +143,7 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,14 +151,15 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter( (auto) => {
+    return auto.type === 'benzina'});
 
 const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
+    return auto.type === 'diesel';
 });
 
 const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+    return auto.type !== 'benzina' && auto.type !== 'diesel';
 });
 
 console.log('Auto a benzina');
